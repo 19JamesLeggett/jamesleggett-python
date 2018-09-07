f5f962297e901a6e8f0e867f11e61454d718dd0f
amerk65584 <amerk65584@gmail.com>
2018-09-07 09:16:56 -0700

    unit 1 program

---
+++ Unit1Lab/jamesleggett.py
@@ -0,0 +1,29 @@
+print("I love Python")
+myName=input()
+print(myName)
+
+print("My name is " + myName)
+
+print(2+2)
+
+
+print(3-2)
+
+print(4*4)
+
+print(3**2)
+
+print(10%3)
+
+print(12/4)
+
+num1=input()
+print(num1)
+
+num2=input()
+
+print(num2)
+
+print(num1 + num2)
+
+print(int(num1)+int(num2))
