47fcf90d4eaf1d5dea7dccbf6cbfa2ddf5c60b3e
amerk65584 <amerk65584@gmail.com>
2018-09-11 09:11:08 -0700

    Unit2Alab

---
+++ Unit2Alab/Unit2Alab.py
@@ -0,0 +1,23 @@
+print(5+4)
+
+print(4.5+ 5)
+
+num1 =input()
+
+num2= input()
+
+print(int(num1)+int(num2))
+
+i=100
+j=200
+x=300
+y=100
+
+print((i == j) or (i == x))
+
+
+print((i<j) and (i>=y))
+
+print((i!=j) and (i != x))
+
+print((i<j) and (i<x) and (i<y))
