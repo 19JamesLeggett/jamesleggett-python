7499afa9ff28e8ff678ef48227023fc54171f4ac
amerk65584 <amerk65584@gmail.com>
2018-10-04 09:15:57 -0700

    unit4Alab

--- Unit4Alab/Unit4Alab.py
+++ Unit4Alab/Unit4Alab.py
@@ -1,3 +1,18 @@
-list1=input()
-print(list1)
-deVowel=list1
+def main():
+    myString = str(input("Enter any string - "))
+    print ("Your string is -" + myString)
+
+    print ("Your string without vowels is -" + deVowel(myString))
+
+def deVowel(myString):
+    myString2 = myString
+    myStringV = ""
+    for x in myString2:
+        if (x!= "a" and x!= "e" and x!= "i" and x!= "o" and x!= "u" and x!= "A" and x!= "E" and x!= "U" and x!= "I" and x!= "O") :
+            myStringV = myStringV + x
+
+    return (myStringV)
+
+
+main()
+
