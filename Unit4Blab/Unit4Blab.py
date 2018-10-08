3440989d4a51643dc422b83d6041a98c02eb6583
amerk65584 <amerk65584@gmail.com>
2018-10-08 09:12:12 -0700

    Unit4Blab

---
+++ Unit4Blab/Unit4Blab.py
@@ -0,0 +1,15 @@
+for i in range(0,5):
+    print(i)
+    answer=(i+10)
+    skrt= (i*10)
+    print("pause")
+    print( str(i) + "+ 10 =" + str(answer))
+    print( str(i) + "*10 =" + str(skrt))
+    print("pause")
+
+myList= [10,20,30,40,50]
+
+for a in range(0, len(myList)):
+    list=(a *10)
+    myList.append(list)
+    print(myList)
