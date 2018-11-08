f50e09c239d48aa772bd5a5bcfb9b00ce641c830
amerk65584 <amerk65584@gmail.com>
2018-11-08 08:53:04 -0800

    DoubleDiceLab

--- DoubleDiceLab/DoubleDiceLab.py
+++ DoubleDiceLab/DoubleDiceLab.py
@@ -1,48 +1,104 @@
-from random import *
+import random
+
+ROLLNUM = 2
+
 def dice():
     loop = "y"
-    list = ['|       |', '|   *   |', '|  * *  |']
+
+    #setOfDice = createDice()
+    #printDiceSideBySide(setOfDice)
     times = 0
     while loop== "y":
-        times = times + 1
-        y = randnum()
-        print(whichDice(y,list))
-        print ('you rolled a ' + str(y))
+        diceList = [0]*ROLLNUM
+        #for i in range(ROLLNUM):
+        (roll, roll2) = randnum()
+        print(roll, roll2)
+        input()
+        diceList = createDice(roll, roll2)
+
+        printDiceSideBySide(diceList)
+            #printDiceSideBySide(diceSet)
+        times += 1
+
+        print ('you rolled a ' + str(roll) + " and a " +str(roll2))
         loop = input('would you like to play again (y/n)')
         print('you have played ' + str(times) + " times")
 
+
+
 def randnum():
-    x = randint(1,6)
-    return(x)
+    x = random.randint(1,5)
+    y = random.randint(1,5)
+    return(x,y)
 
-def whichDice(randnum , list):
-    a = "---------"
-    e = "---------"
+def createDice(roll, roll2):
+   topBot = ' ------- '
+   blank1  = '|       |'
+   blankL  = '| *     |'
+   blankR  = '|     * |'
+   blankM  = '|   *   |'
+   blank2  = '|  * *  |'
+   one = [topBot, blank1, blankM, blank1, topBot]
+   two = [topBot, blank1, blank2, blank1, topBot]
+   three = [topBot, blankL, blankM, blankR, topBot]
+   four = [topBot, blank2, blank1, blank2, topBot]
+   five = [topBot, blank2, blankM, blank2, topBot]
+   six = [topBot, blank2, blank2, blank2, topBot]
 
-    if randnum == 1 or randnum == 2:
-        b = list[0]
-    elif randnum == 3:
-        b = list[1]
-    elif randnum == 4 or randnum == 5 or randnum == 6:
-        b = list[2]
+   if roll == 1:
+       roll = one
 
-    if randnum == 1 or randnum == 3 or randnum == 5:
-        c = list[1]
-    elif randnum == 4:
-        c = list[0]
-    elif randnum == 2 or randnum == 6:
-        c = list[2]
+   elif roll == 2:
+       roll = two
 
-    if randnum == 1 or randnum == 2:
-        d = list[0]
-    elif randnum == 3:
-        d = list[1]
-    elif randnum == 4 or randnum == 5 or randnum == 6:
-        d = list[2]
+   elif roll == 3:
+       roll = three
+
+   elif roll == 4:
+       roll = four
+
+   elif roll == 5:
+       roll = five
+
+   elif roll == 6:
+       roll = six
+
+   if roll2 == 1:
+       roll2 = one
+
+   elif roll2 == 2:
+       roll2 = two
+
+   elif roll2 == 3:
+       roll2 = three
+
+   elif roll2 == 4:
+       roll2 = four
+
+   elif roll2 == 5:
+       roll2 = five
+
+   elif roll2 == 6:
+       roll2 = six
+
+   else:
+       print('error')
+
+   Dice1 = roll
+   Dice2 = roll2
 
 
 
+   allDice = [Dice1, Dice2]
 
-    return(a + "\n" + b + "\n" + c + "\n" + d + "\n" + e)
+
+   return allDice
+
+def printDiceSideBySide(allDice):
+     for row in range(0,len(allDice[0])):
+         for col in range(0,len(allDice)):
+             print(allDice[col][row], end = '\t')
+         print()
+
 
 dice()
